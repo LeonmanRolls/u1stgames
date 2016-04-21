516228e6cbfc6403ad4f24234064d0635bab0696
Leon Talbert <leon.tabert@gmai.com>
2016-04-22 07:13:11 +0800

    styling

--- src/cljs/u1stgames/core.cljs
+++ src/cljs/u1stgames/core.cljs
@@ -4,13 +4,45 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(def
+  app-state
+  (atom {:games [{:img "/img/u1st_logo_square.png"}
+                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xtp1/t39.2082-0/p528x396/12679461_1065986690126640_622421424_n.jpg"}
+                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xap1/t39.2082-0/p528x396/12532990_208055976241061_513677896_n.jpg"}
+                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xfa1/t39.2082-0/p528x396/12521777_822095034567909_245141124_n.jpg"}
+                 {:img "https://external-tpe1-1.xx.fbcdn.net/safe_image.php?d=AQDHfbzhSPvVuH1a&w=400&h=225&url=https%3A%2F%2Fscontent-tpe1-1.xx.fbcdn.net%2Fhphotos-xal1%2Ft39.2082-0%2Fp528x396%2F12350990_989179941141650_728204544_n.jpg&cfs=1&blur=0"}
+                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xtp1/t39.2082-0/p528x396/12679461_1065986690126640_622421424_n.jpg"}
+                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xap1/t39.2082-0/p528x396/12532990_208055976241061_513677896_n.jpg"}
+                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xfa1/t39.2082-0/p528x396/12521777_822095034567909_245141124_n.jpg"}
+                 {:img "https://external-tpe1-1.xx.fbcdn.net/safe_image.php?d=AQDHfbzhSPvVuH1a&w=400&h=225&url=https%3A%2F%2Fscontent-tpe1-1.xx.fbcdn.net%2Fhphotos-xal1%2Ft39.2082-0%2Fp528x396%2F12350990_989179941141650_728204544_n.jpg&cfs=1&blur=0"}
+                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xtp1/t39.2082-0/p528x396/12679461_1065986690126640_622421424_n.jpg"}
+                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xap1/t39.2082-0/p528x396/12532990_208055976241061_513677896_n.jpg"}
+                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xfa1/t39.2082-0/p528x396/12521777_822095034567909_245141124_n.jpg"}
+                 {:img "https://external-tpe1-1.xx.fbcdn.net/safe_image.php?d=AQDHfbzhSPvVuH1a&w=400&h=225&url=https%3A%2F%2Fscontent-tpe1-1.xx.fbcdn.net%2Fhphotos-xal1%2Ft39.2082-0%2Fp528x396%2F12350990_989179941141650_728204544_n.jpg&cfs=1&blur=0"}
+                 ]}))
 
-(defn root-component [app owner]
+(defn block-li [{:keys [img]} owner]
+ (reify
+  om/IRender
+   (render [_]
+    (dom/li #js {:style #js {:backgroundImage (str "url(" img ")")
+                             :backgroundSize "cover"
+                             :backgroundRepeat "no-repeat"
+                             }} )
+
+     )
+   )
+  )
+
+
+(defn root-component [{:keys [games]} owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div nil (dom/h1 nil (:text app))))))
+      (dom/ul nil
+             (om/build-all block-li games)
+              )
+      )))
 
 (om/root
  root-component
