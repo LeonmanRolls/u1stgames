a6b67bb5213ed96b99ea71b915ee69b31aaf9a0c
Leon Talbert <leon.tabert@gmai.com>
2016-04-25 16:14:11 +0800

    mouse over bg image

--- src/cljs/u1stgames/core.cljs
+++ src/cljs/u1stgames/core.cljs
@@ -22,96 +22,124 @@
 (defn chan-get [url chan]
   (GET url {:handler #(put! chan (cljs.reader/read-string %))}))
 
-(comment
-
-  (GET "/fbgames" {:handler handler})
-
-  (.api js/FB "/1557991804501532" "get" #js {} #(println %))
-
-  )
-
 (defn ^:export youtubeReady []
   (put! yt-init-chan {:msg-type :init}))
 
 (def
   app-state
-  (atom {:games [{:img "/img/u1st_logo_square.png"}
-                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xtp1/t39.2082-0/p528x396/12679461_1065986690126640_622421424_n.jpg"}
-                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xap1/t39.2082-0/p528x396/12532990_208055976241061_513677896_n.jpg"}
-                 {:img "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xfa1/t39.2082-0/p528x396/12521777_822095034567909_245141124_n.jpg"}
+  (atom {:games [{:picture "/img/u1st_logo_square.png"}
+                 {:title ""
+                  :subcategory ""
+                  :appid ""
+                  :id ""
+                  :picture "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xap1/t39.2082-0/p528x396/12532990_208055976241061_513677896_n.jpg"
+                  :monthly_active_user ""
+                  :ytvideo ""}
+                 {:picture "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xap1/t39.2082-0/p528x396/12532990_208055976241061_513677896_n.jpg"}
+                 {:picture "https://scontent-tpe1-1.xx.fbcdn.net/hphotos-xfa1/t39.2082-0/p528x396/12521777_822095034567909_245141124_n.jpg"}
                  ]}))
 
-(defn block-li [{:keys [img]} owner]
+(comment
+
+  (GET "/fbgames" {:handler handler})
+
+  (fn []
+
+
+    )
+
+  (.api js/FB "/1557991804501532" "get" #js {} #(println %))
+
+  )
+
+
+(defn block-li [{:keys [picture]} owner]
  (reify
   om/IRender
    (render [_]
-    (dom/li #js {:style #js {:backgroundImage (str "url(" img ")")
+    (dom/li #js {:style #js {:backgroundImage (str "url(" picture ")")
                              :backgroundSize "cover"
                              :backgroundRepeat "no-repeat"}}))))
 
-(defn img-block [data owner]
+(defn img-block [{:keys [picture] :as data} owner]
   (reify
     om/IInitState
     (init-state [_]
       {:uid (u/uid-gen "yt")
+       :hover-uid (u/uid-gen "hover")
+       :bg-cover-uid (u/uid-gen "bgcover")
        :player {}})
 
     om/IDidMount
     (did-mount [_]
       (let [subscriber (chan)
-            {:keys [uid player]} (om/get-state owner)]
+            {:keys [uid hover-uid bg-cover-uid player]} (om/get-state owner)]
         (sub yt-init-pub :init subscriber)
-        (go
-          (let [inited (<! subscriber)]
-            (om/set-state!
-              owner
-              :player
-              (new js/YT.Player uid #js {:height "300"
-                                         :width "300"
-                                         :videoId "scPbcEUCiec"
-                                         :playerVars #js {:controls 0
-                                                          :showinfo 0
-                                                          :modestbranding 1}
-                                         :events #js {:onReady #(println "ready")
-                                                      :onStateChange #(println "state change")}}))))
+        #_(go
+            (let [inited (<! subscriber)]
+              (om/set-state!
+                owner
+                :player
+                (new js/YT.Player uid #js {:height "300"
+                                           :width "300"
+                                           :videoId "scPbcEUCiec"
+                                           :playerVars #js {:controls 0
+                                                            :showinfo 0
+                                                            :modestbranding 1}
+                                           :events #js {:onReady #(println "ready")
+                                                        :onStateChange #(println "state change")}}))))
         (.hover
-          (js/$ "#lili")
+          (js/$ (str "#" hover-uid))
           #(do
-            (println "mouseover")
             (.animate
-              (js/$ "#bg-cover")
-              #js {:marginTop "-300px"} 500)
+              (js/$ (str "#" bg-cover-uid))
+              #js {:marginTop "-300px"} 200)
             )
 
           #(do
-            (println "mouseOut")
             (.animate
-              (js/$ "#bg-cover")
-              #js {:marginTop "0px"} 500)
+              (js/$ (str "#" bg-cover-uid))
+              #js {:marginTop "0px"} 200)
             )
           )
 
         ))
 
     om/IRenderState
-    (render-state [_ {:keys [uid player]}]
-      (dom/li #js {:id "lili"
-                   :onMouseOver #(.playVideo player)
-                   :onMouseOut #(.pauseVideo player)}
+    (render-state [_ {:keys [uid hover-uid bg-cover-uid player]}]
+      (dom/li #js {:id hover-uid
+                   ;  :onMouseOver #_(.playVideo player)
+                   ; :onMouseOut #_(.pauseVideo player)
+                   }
               (dom/div #js {:id uid})
-              (dom/div #js {:id "bg-cover"})))))
+              (dom/div #js {:id bg-cover-uid
+                            :className "bg-cover"
+                            :style #js {:backgroundImage (str "url(" picture ")")
+                                        :backgroundSize "cover"
+                                        :backgroundRepeat "no-repeat"
+                                        :backgroundColour "blue"}})))))
 
 (defn root-component [{:keys [games]} owner]
   (reify
+
+    om/IDidMount
+    (did-mount [_]
+
+      (GET
+        "/fbgames"
+        {:handler (fn [all-games]
+                    (let [read-games (cljs.reader/read-string all-games)]
+                      (om/transact!
+                        games
+                        (fn [games]
+                          #_(println "conj: " (conj games read-games))
+                          (into games read-games)))))}))
+
     om/IRender
     (render [_]
       (dom/ul nil
-             (om/build img-block {})
-
-             (om/build-all block-li games)
-
-              )
-      )))
+              (om/build-all img-block games)
+              (om/build-all block-li games)))))
 
 (om/root
  root-component
